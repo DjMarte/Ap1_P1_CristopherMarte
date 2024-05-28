16b9dd663a8fdf30176de7bf63e2010f457f2c8e
DjMarte <alphat098@gmail.com>
2024-05-27 20:55:06 -0400

    Creé la carpeta DAL y su contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ap1_P1_CristopherMarte.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
