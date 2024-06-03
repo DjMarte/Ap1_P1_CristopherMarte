d00f1f23f4a3fb8f2c0e97f396fed9c7ebf0d943
DjMarte <alphat098@gmail.com>
2024-06-03 18:42:17 -0400

    Agregué los Articulos al Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ap1_P1_CristopherMarte.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Ap1_P1_CristopherMarte.DAL;
 
 public class Contexto : DbContext
 {
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Articulos> Articulos { get; set; }
 }
