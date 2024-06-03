18b238c8207d733e19e5a277136eca50f76daf56
DjMarte <alphat098@gmail.com>
2024-06-03 18:40:38 -0400

    Creé el modelo Articulos

---
+++ Models/Articulos.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ap1_P1_CristopherMarte.Models;
+
+//Precio = (Costo * %)/100 + costo
+
+public class Articulos
+{
+    [Key]
+    public int ArticuloId { get; set; }
+
+    [RegularExpression(@"^[a-zA-z\s]+$", ErrorMessage = "Solo se permiten letras")]
+    [Required(ErrorMessage = "Descripcion obligatorio")]
+    public string? Descripcion { get; set; }
+
+    [RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
+    [Range(0.01, 1000000, ErrorMessage = "Ingrese un valor mayor a 0 y menor o igual a 1000000")]
+    [Required(ErrorMessage = "Costo obligatorio")]
+    public decimal Costo{ get; set; }
+
+    [Required(ErrorMessage = "Ganancia obligatoria")]
+	[RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
+	public decimal Ganancia { get; set; }
+
+	[RegularExpression(@"^[0-9]+$", ErrorMessage = "Solo se permiten numeros")]
+	[Range(0.01, 1000000, ErrorMessage = "Ingrese un valor mayor a 0 y menor o igual a 1000000")]
+    [Required(ErrorMessage = "Precio obligatorio")]
+    public decimal Precio { get; set; }
+}
